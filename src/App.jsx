aeba9b33c4f19f4def18df27e6416df2ae210d67
adityapetshali <adityapetshali@outlook.com>
2025-03-04 06:52:10 +0530

    Added navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,15 @@
+import Navbar from "./Components/Navbar"
+
 const App = () => {
   return (
-    <div className=''>Hello World</div>
+    <div className='px-4 md:px-8 lg:px-32 2xl:px-64'>
+    {/* NAVBAR */}
+    <Navbar/>
+    {/* BREADCRUMB */}
+    {/* INTRODUCTION */}
+    {/* FEATURED POST */}
+    {/* POST LIST */}  
+    </div>
   )
 }
 
