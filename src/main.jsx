aab80d35d621f73234216be30d2c037c36cc062d
adityapetshali <adityapetshali@outlook.com>
2025-03-05 07:36:40 +0530

    added react router dom for navigation

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,49 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Homepage from "./Routes/Homepage.jsx";
+import PostListPage from "./Routes/PostListPage.jsx";
+import Write from "./Routes/Write.jsx";
+import LoginPage from "./Routes/LoginPage.jsx";
+import RegisterPage from "./Routes/RegisterPage.jsx";
+import SinglePostPage from "./Routes/SinglePostPage.jsx";
+import MainLayout from "./layouts/MainLayout.jsx";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    element:<MainLayout/>,
+    children:[
+      {
+        path: "/",
+        element: <Homepage />,
+      },
+      {
+        path: "/posts",
+        element: <PostListPage />,
+      },
+      {
+        path: "/:slug",
+        element: <SinglePostPage />,
+      },
+      {
+        path: "/write",
+        element: <Write />,
+      },
+      {
+        path: "/login",
+        element: <LoginPage />,
+      },
+      {
+        path: "/register",
+        element: <RegisterPage />,
+      },
+    ]
+  }
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
